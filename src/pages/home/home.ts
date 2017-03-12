8727b1e8f06bd2a2d1c5396515b9a7aec6f25b57
Radu Jinga <jinga.radu@gmail.com>
2017-03-12 02:55:28 +0200

    minor changes for consistency of coding style

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
 import { AuthService } from '../../providers/auth-service';
 import { LoginPage } from '../login/login';
 import { Storage } from '@ionic/storage';
