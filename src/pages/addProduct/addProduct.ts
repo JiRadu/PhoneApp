b4f75fbb212399124d3c040b7e21d87ff59b85a6
Radu Jinga <jinga.radu@gmail.com>
2017-03-14 13:35:31 +0200

    added verification in constructor if first response is null, now application works

--- src/pages/addProduct/addProduct.ts
+++ src/pages/addProduct/addProduct.ts
@@ -3,52 +3,41 @@ import {NavController} from 'ionic-angular';
 import { AuthService } from '../../providers/auth-service';
 import { LoginPage } from '../login/login';
 import { Storage } from '@ionic/storage';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+
 @Component({
   selector: 'add-product',
   templateUrl: 'addProduct.html',
   providers: [Storage]
 })
+
 export class AddProductPage {
   name = '';
   email = '';
-  productCredentials = {};
-  Product = {
+  Products = [{
     nume: '',
     cost: ''
-  };
-  Products = [this.Product];
+  }];
   authForm = new FormGroup({
        nume: new FormControl(),
        cost: new FormControl()
     });
 
-
-
-
   constructor(private nav: NavController, private auth: AuthService, public storage: Storage) {
 
-    //TODO:Figure out if we need verification is user is not auth here
     let info = this.auth.getUserInfo();
     this.name = info.name;
     this.email = info.email;
     this.storage = storage;
     this.storage.get('productList').then((response)=>{
-      this.Products = response;
+      if(response === null){
+        this.Products = [];
+      } else {
+        this.Products = response;
+      }
     });
   }
-  public testAddProduct(){
-  //   let storage = this.storage;
-  //   storage.get('productList').then(function(response){
-  //     if(response == null){
-  //       response = [];
-  //     }
-  //     response.push({nume:"pizza",cost:3});
-  //     console.log(storage);
-  //     storage.set('productList',response);
-  //     console.log(response);
-  //   });
-  }
+
   public setData(key,value){
     this.storage.set(key,value);
   }
@@ -59,9 +48,6 @@ export class AddProductPage {
     });
 
   }
-
-
-
   public logout() {
 
     this.auth.logout().subscribe(succ => {
@@ -70,6 +56,7 @@ export class AddProductPage {
         this.nav.setRoot(LoginPage);
     });
   }
+
   public deleteProduct(product){
     let storage = this.storage;
     this.Products.splice(this.Products.indexOf(product));
@@ -78,6 +65,7 @@ export class AddProductPage {
       storage.set('productList',response);
     });
   }
+
   public submitForm(Product){
     let storage = this.storage;
 
@@ -90,4 +78,5 @@ export class AddProductPage {
       storage.set('productList',response);
     });
   }
+
 }
